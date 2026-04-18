cc2acc82fa7a3514127383a14b18f635f4236fd6
dachi <ani10sanadze@gmail.com>
2026-04-18 06:43:32 +0400

    fix: resolve ng2-charts version conflict

--- src/app/features/auth/login/pages/login-page/login-page.ts
+++ src/app/features/auth/login/pages/login-page/login-page.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { AuthInput } from "../../../../../shared/components/auth-input/auth-input";
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ValidationService } from '../../../../../core/services/auth-services/validation-service';
+import { LogoIcon } from "../../../../../shared/icons/logo-icon/logo-icon";
 
 @Component({
   selector: 'app-login-page',
-  imports: [AuthInput],
+  imports: [AuthInput, ReactiveFormsModule, LogoIcon],
   templateUrl: './login-page.html',
   styleUrl: './login-page.css',
 })
 export class LoginPage {
+
+  private validationService = inject(ValidationService);
+
+  public form = new FormGroup({
+    email: new FormControl ('', [
+      this.validationService.required,
+      this.validationService.emailValidation
+    ]),
+    password: new FormControl ('', [
+      this.validationService.required,
+      this.validationService.passwordValidation
+    ])
+  }, {
+    updateOn: 'submit'
+  })
 
 }
--- src/app/features/auth/register/pages/register-page/register-page.ts
+++ src/app/features/auth/register/pages/register-page/register-page.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { LogoIcon } from "../../../../../shared/icons/logo-icon/logo-icon";
+import { AuthInput } from "../../../../../shared/components/auth-input/auth-input";
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ValidationService } from '../../../../../core/services/auth-services/validation-service';
 
 @Component({
   selector: 'app-register-page',
-  imports: [],
+  imports: [ LogoIcon, AuthInput, ReactiveFormsModule],
   templateUrl: './register-page.html',
   styleUrl: './register-page.css',
 })
 export class RegisterPage {
+
+  private validationService = inject(ValidationService);
+
+  public form = new FormGroup({
+    username: new FormControl('', [
+      this.validationService.required,
+      this.validationService.usernameValidation
+    ]),
+    lastname: new FormControl('', [
+      this.validationService.required,
+      this.validationService.usernameValidation
+    ]),
+    email: new FormControl('', [
+      this.validationService.required,
+      this.validationService.emailValidation
+    ]),
+    number: new FormControl('', [
+      this.validationService.required,
+      this.validationService.numberValidation
+    ]),
+    password: new FormControl('', [
+      this.validationService.required,
+      this.validationService.passwordValidation
+    ]),
+    confirm: new FormControl(''),
+  },{
+    updateOn: 'submit'
+  })
 
 }
