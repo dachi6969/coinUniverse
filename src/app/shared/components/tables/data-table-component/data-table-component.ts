88b6f3b03745f280d896f5abbee7808b0d28d1cb
dachi <ani10sanadze@gmail.com>
2026-05-22 16:37:33 +0400

    feat(table): add reusable data table

---
+++ src/app/shared/components/tables/data-table-component/data-table-component.ts
@@ -0,0 +1,23 @@
+import { Component, contentChildren, input, TemplateRef } from '@angular/core';
+import { TableColumnDirective } from '../../../directives/table-column.directive';
+import { CommonModule } from '@angular/common';
+import { TableSkeleton } from "../../../../features/dashboard/components/top-performers-table/table-skeleton/table-skeleton";
+
+@Component({
+  selector: 'data-table-component',
+  imports: [CommonModule, TableSkeleton],
+  templateUrl: './data-table-component.html',
+  styleUrl: './data-table-component.css',
+})
+export class DataTableComponent {
+  public readonly data = input<any[]>();
+  public readonly columns = input<any[]>();
+
+  private readonly customTemplates = 
+  contentChildren(TableColumnDirective);
+
+  getTemplate(key: string): TemplateRef<any> | null {
+    const match = this.customTemplates().find(t => t.name() === key);
+    return match ? match.templateRef : null;
+  }
+}
