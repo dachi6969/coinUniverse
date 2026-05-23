d3fe5f7abe0509d1fecbc15449e43461dc58a488
dachi <ani10sanadze@gmail.com>
2026-05-23 17:07:01 +0400

    refactor(table): replace hardcoded top-exchanges table with shared one

--- src/app/features/dashboard/components/top-exchanges-table/top-exchanges-table.ts
+++ src/app/features/dashboard/components/top-exchanges-table/top-exchanges-table.ts
@@ -1,14 +1,27 @@
 import { Component, input } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { CryptoExchange } from '../../../../core/types/coin-types';
-import { TableSkeleton } from "../top-performers-table/table-skeleton/table-skeleton";
+import { DataTableComponent } from "../../../../shared/components/tables/data-table-component/data-table-component";
+import { TableColumnDirective } from "../../../../shared/directives/table-column.directive";
 
 @Component({
   selector: 'top-exchanges-table',
-  imports: [CommonModule, NgOptimizedImage, TableSkeleton],
+  imports: [
+    CommonModule,
+    NgOptimizedImage,
+    DataTableComponent,
+    TableColumnDirective
+],
   templateUrl: './top-exchanges-table.html',
   styleUrl: './top-exchanges-table.css',
 })
 export class TopExchangesTable {
   public readonly data = input< CryptoExchange[] | null >(null);
+
+  public readonly topExchangesColumns = [
+    { key: 'name', label: 'Exchange' },
+    { key: 'trust_score', label: 'Trust Score' },
+    { key: 'trade_volume_24h_btc', label: '24h Volume (BTC)' },
+    { key: 'country_year', label: 'Country / year' },
+  ]
 }
