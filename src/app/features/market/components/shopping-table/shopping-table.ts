85dd24c472284bc3e868ec2ad189ec9e6ab75542
dachi <ani10sanadze@gmail.com>
2026-05-23 06:26:50 +0400

    feat(table): create coins shop list table with buy functional

--- src/app/features/market/components/shopping-table/shopping-table.ts
+++ src/app/features/market/components/shopping-table/shopping-table.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
+import { Coin } from '../../../../core/types/coin-types';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { PriceHighlightDirective } from '../../../../shared/directives/price-highlight-directive';
+import { BenefitIndicator } from '../../../../shared/directives/benefit-indicator';
+import { AbbreviateNumberPipe } from '../../../../shared/pipes/abbreviate-number/abbreviate-number-pipe';
+import { UiButton } from "../../../../shared/components/ui-button/ui-button";
+import { DataTableComponent } from "../../../../shared/components/tables/data-table-component/data-table-component";
+import { TableColumnDirective } from '../../../../shared/directives/table-column.directive';
 
 @Component({
   selector: 'shopping-table',
-  imports: [],
+  imports: [
+    CommonModule,
+    NgOptimizedImage,
+    PriceHighlightDirective,
+    BenefitIndicator,
+    AbbreviateNumberPipe,
+    UiButton,
+    DataTableComponent,
+    TableColumnDirective,
+],
   templateUrl: './shopping-table.html',
   styleUrl: './shopping-table.css',
 })
 export class ShoppingTable {
+  public readonly topCoins = input< Coin[] | null >(null);
+  public readonly balance = input< number >(0);
+  public readonly onBuy = output<Coin>();
 
+  public readonly shopTableColumns = [
+    { key: 'symbol', label: 'Coin' },
+    { key: 'current_price', label: 'Price' },
+    { key: 'price_change_percentage_24h', label: '24h Change' },
+    { key: 'market_cap', label: 'Market Cap' },
+    { key: 'actions', label: 'Actions' },
+  ];
 }
