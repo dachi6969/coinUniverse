700009db00ec1882ab4c3e0c19977a97f781059f
dachi <ani10sanadze@gmail.com>
2026-05-23 06:29:35 +0400

    feat(market-page): wire up child components and connect streams

--- src/app/features/market/pages/market-page/market-page.ts
+++ src/app/features/market/pages/market-page/market-page.ts
@@ -1,12 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ShoppingTable } from "../../components/shopping-table/shopping-table";
+import { CommonModule } from '@angular/common';
+import { BuyCoinPayload, Coin, OwnedCoins } from '../../../../core/types/coin-types';
+import { ShopDialog } from '../../components/shop-dialog/shop-dialog';
+import { BoughtCoinsTable } from "../../components/bought-coins-table/bought-coins-table";
+import { MarketService } from '../../services/market-service';
+import { SellCoinDialog } from "../../components/sell-coin-dialog/sell-coin-dialog";
 
 @Component({
   selector: 'app-market-page',
-  imports: [ShoppingTable],
+  imports: [
+    ShoppingTable,
+    CommonModule,
+    ShopDialog,
+    BoughtCoinsTable,
+    SellCoinDialog
+],
   templateUrl: './market-page.html',
   styleUrl: './market-page.css',
 })
 export class MarketPage {
 
+  private readonly marketService = inject(MarketService);
+  public readonly selectedCoin = this.marketService.selectedCoin;
+  public readonly selectedCoinToSell = this.marketService.selectedCoinToSell;
+  public readonly loading = this.marketService.loading;
+  public readonly userData = this.marketService.userData;
+  
+  public readonly topCoins$ = 
+  this.marketService.topCoins$;
+
+  public readonly boughtCoins$ =
+  this.marketService.allCoins$;
+  
+  public readonly livePrice$ = 
+  this.marketService.livePrice$;
+
+  public readonly livePricesToSell$ = 
+  this.marketService.livePriceToSell$;
+
+  // METHODS.
+  public selectCoinToBuy(c: Coin) {
+    this.marketService.selectCoinToBuy(c);
+  };
+  public selectCoinToSell(c: OwnedCoins) {
+    this.marketService.selectCoinToSell(c);
+  };
+
+  public exitBuyProcess() {
+    this.marketService.exitBuyProcess();
+  };
+  public exitSellProcess() {
+    this.marketService.exitSellProcess();
+  };
+
+  public async onCoinTransfer(readyCoin: BuyCoinPayload) {
+    this.marketService.addCoinInfo(readyCoin);
+  };
 }
