c1e42819de8655b1b1a9b014623cadf0365b69ba
dachi <ani10sanadze@gmail.com>
2026-05-23 06:17:58 +0400

    feat(table): create bought coins table

---
+++ src/app/features/market/components/bought-coins-table/bought-coins-table.ts
@@ -0,0 +1,36 @@
+import { Component, input, output } from '@angular/core';
+import { OwnedCoins } from '../../../../core/types/coin-types';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { BenefitIndicatorPipe } from '../../../../shared/pipes/benefit-indicator-pipe';
+import { UiButton } from "../../../../shared/components/ui-button/ui-button";
+import { PriceHighlightDirective } from '../../../../shared/directives/price-highlight-directive';
+import { DataTableComponent } from "../../../../shared/components/tables/data-table-component/data-table-component";
+import { TableColumnDirective } from "../../../../shared/directives/table-column.directive";
+
+@Component({
+  selector: 'bought-coins-table',
+  imports: [
+    CommonModule,
+    NgOptimizedImage,
+    BenefitIndicatorPipe,
+    UiButton,
+    PriceHighlightDirective,
+    DataTableComponent,
+    TableColumnDirective
+],
+  templateUrl: './bought-coins-table.html',
+  styleUrl: './bought-coins-table.css',
+})
+export class BoughtCoinsTable {
+  public readonly boughtCoins = input< OwnedCoins[] | null >(null);
+  public readonly onSell = output<OwnedCoins>();
+
+  public readonly boughtCoinsTableColumns = [
+    { key: 'coin', label: 'Coin' },
+    { key: 'buy_price', label: 'Avg. Buy Price' },
+    { key: 'current_price', label: 'Current Price' },
+    { key: 'amount', label: 'Amount' },
+    { key: 'profitLoss', label: 'Profit / Loss' },
+    { key: 'actions', label: 'Actions' },
+  ];
+}
