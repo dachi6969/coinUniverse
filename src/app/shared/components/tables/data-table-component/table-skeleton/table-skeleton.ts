5c678a570999a8a505483f0ef0c3fbf8e19eb837
dachi <ani10sanadze@gmail.com>
2026-05-23 16:48:43 +0400

    fix(skeleton): replace skeleton folder

---
+++ src/app/shared/components/tables/data-table-component/table-skeleton/table-skeleton.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'table-skeleton',
+  imports: [],
+  templateUrl: './table-skeleton.html',
+  styleUrl: './table-skeleton.css',
+})
+export class TableSkeleton {
+
+}
