714fec0dffb6718f5e724c2ab93d293e4d3a39d5
dachi <ani10sanadze@gmail.com>
2026-05-27 17:49:32 +0400

    feat(table): add active assets table component

---
+++ src/app/features/portfolio/components/avalible-assets/avalible-assets.ts
@@ -0,0 +1,33 @@
+import { Component, input } from '@angular/core';
+import { DataTableComponent } from "../../../../shared/components/tables/data-table-component/data-table-component";
+import { OwnedCoins } from '../../../../core/types/coin-types';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { PriceHighlightDirective } from '../../../../shared/directives/price-highlight-directive';
+import { BenefitIndicatorPipe } from '../../../../shared/pipes/benefit-indicator-pipe';
+import { TableColumnDirective } from "../../../../shared/directives/table-column.directive";
+
+@Component({
+  selector: 'avalible-assets',
+  imports: [
+    DataTableComponent,
+    CommonModule,
+    PriceHighlightDirective,
+    BenefitIndicatorPipe,
+    NgOptimizedImage,
+    TableColumnDirective
+],
+  templateUrl: './avalible-assets.html',
+  styleUrl: './avalible-assets.css',
+})
+export class AvalibleAssets {
+  public readonly ownCoins = input<OwnedCoins[] | null>(null);
+
+  public readonly ownCoinsTableColumns = [
+    { key: 'coin', label: 'Coin' },
+    { key: 'buy_price', label: 'Avg. Buy Price' },
+    { key: 'current_price', label: 'Current Price' },
+    { key: 'amount', label: 'Amount' },
+    { key: 'profitLoss', label: 'Profit / Loss' },
+  ];
+
+}
