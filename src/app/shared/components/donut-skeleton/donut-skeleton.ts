942d336d476d48e8d233b8a2a80b9e599c100f4b
dachi <ani10sanadze@gmail.com>
2026-05-27 17:42:44 +0400

    fear(skeleton): create donut skeleton

---
+++ src/app/shared/components/donut-skeleton/donut-skeleton.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'donut-skeleton',
+  imports: [],
+  templateUrl: './donut-skeleton.html',
+  styleUrl: './donut-skeleton.css',
+})
+export class DonutSkeleton {
+
+}
