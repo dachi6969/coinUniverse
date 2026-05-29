2a07ed086d0a5e2246e16323d700f930bf28b4b8
dachi <ani10sanadze@gmail.com>
2026-05-29 13:33:03 +0400

    feat(cards): create portfolio highlight cards, with nested single custom card

---
+++ src/app/features/portfolio/components/highlight-cards/highlight-card/highlight-card.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'highlight-card',
+  imports: [],
+  templateUrl: './highlight-card.html',
+  styleUrl: './highlight-card.css',
+})
+export class HighlightCard {}
---
+++ src/app/features/portfolio/components/highlight-cards/highlight-cards.ts
@@ -0,0 +1,41 @@
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { Component, computed, input } from '@angular/core';
+import { PriceHighlightDirective } from '../../../../shared/directives/price-highlight-directive';
+import { HighlightSkeleton } from "../../../dashboard/components/dashboard-highlights/highlight-skeleton/highlight-skeleton";
+import { HighlightCard } from "./highlight-card/highlight-card";
+import { PortfolioHighlightsType } from '../../../../core/types/highlights-types';
+
+@Component({
+  selector: 'highlight-cards',
+  imports: [
+    CommonModule, 
+    PriceHighlightDirective, 
+    NgOptimizedImage, 
+    HighlightSkeleton, 
+    HighlightCard
+  ],
+  templateUrl: './highlight-cards.html',
+  styleUrl: './highlight-cards.css',
+})
+export class HighlightCards {
+  public readonly balance = input<number>(0);
+  public readonly highlightsData = input< PortfolioHighlightsType | null>(null);
+
+  public readonly profit = computed(() => {
+    const data = this.highlightsData();
+    if ( !data ) return null;
+    return data.profit ?? 0.00;
+  });
+  public readonly coinsQuaintity = computed(() => {
+    const data = this.highlightsData();
+    if ( !data ) return null;
+    return data.avalibleCoinsQuaintity ?? 0;
+  });
+  public readonly topPerformer = computed(() => {
+    const data = this.highlightsData();
+    if ( !data ) return null;
+    return data.topPerformer;
+  });
+  
+
+}
