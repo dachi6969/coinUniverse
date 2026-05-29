ba8ec84d1eb05f607b82d222e5859a81a3ca73c3
dachi <ani10sanadze@gmail.com>
2026-05-29 13:44:18 +0400

    feat(transactions-history): create transactions history components, with custom cards

---
+++ src/app/features/portfolio/components/transaction-history/transaction-history.ts
@@ -0,0 +1,28 @@
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { Component, input } from '@angular/core';
+import { OwnedCoins } from '../../../../core/types/coin-types';
+
+@Component({
+  selector: 'transaction-history',
+  imports: [CommonModule, NgOptimizedImage],
+  templateUrl: './transaction-history.html',
+  styleUrl: './transaction-history.css',
+})
+export class TransactionHistory {
+  public readonly transactionHistory = input<OwnedCoins[] | null>(null);
+
+  public getTransactionTotal(c: OwnedCoins): number {
+    const amount = c.status === 'sold'
+      ? c.amount * -1
+      : c.amount;
+  
+    return c.buy_price * amount;
+  };
+
+  public isSold (c: OwnedCoins): boolean {
+    return c.status === 'sold';
+  };
+  public statusLabel (c: OwnedCoins): string {
+    return c.status === 'sold' ? 'Sold' : 'Bought'
+  };
+}
