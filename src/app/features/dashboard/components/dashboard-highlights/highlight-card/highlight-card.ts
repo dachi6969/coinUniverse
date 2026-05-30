15352e1fbf48cf1a29f4a387b6eccd497920feca
dachi <ani10sanadze@gmail.com>
2026-05-30 06:34:59 +0400

    perf(card): add NgOptimizedImage in custom highlight card

--- src/app/features/dashboard/components/dashboard-highlights/highlight-card/highlight-card.ts
+++ src/app/features/dashboard/components/dashboard-highlights/highlight-card/highlight-card.ts
@@ -1,12 +1,12 @@
 import { Component, input } from '@angular/core';
 import { Coin } from '../../../../../core/types/coin-types';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { PriceHighlightDirective } from '../../../../../shared/directives/price-highlight-directive';
 import { BenefitIndicator } from "../../../../../shared/directives/benefit-indicator";
 
 @Component({
   selector: 'highlight-card',
-  imports: [CommonModule, PriceHighlightDirective, BenefitIndicator],
+  imports: [CommonModule, PriceHighlightDirective, BenefitIndicator,NgOptimizedImage],
   templateUrl: './highlight-card.html',
   styleUrl: './highlight-card.css',
 })
