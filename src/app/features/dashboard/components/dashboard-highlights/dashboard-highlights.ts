647d1a81bd3591753778f248171822adbf13a6f8
dachi <ani10sanadze@gmail.com>
2026-05-30 06:11:30 +0400

    refactor(highlight-cards): separate highlight data into custom card component, refine data in dumb component

--- src/app/features/dashboard/components/dashboard-highlights/dashboard-highlights.ts
+++ src/app/features/dashboard/components/dashboard-highlights/dashboard-highlights.ts
@@ -2,18 +2,18 @@ import { Component, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HighlightSkeleton } from "./highlight-skeleton/highlight-skeleton";
 import { DashboardData } from '../../../../core/types/coin-types';
-import { PriceHighlightDirective } from "../../../../shared/directives/price-highlight-directive";
-import { LivePrices } from '../../../../core/types/live-prices.types';
+import { HighlightCard } from "./highlight-card/highlight-card";
 
 @Component({
   selector: 'dashboard-highlights',
-  imports: [CommonModule, HighlightSkeleton, PriceHighlightDirective],
+  imports: [
+    CommonModule, 
+    HighlightSkeleton, 
+    HighlightCard
+  ],
   templateUrl: './dashboard-highlights.html',
   styleUrl: './dashboard-highlights.css',
 })
 export class DashboardHighlights {
-
-  readonly data = input< DashboardData | null >(null);
-  readonly livePrices = input< LivePrices | null >(null);
-
+  public readonly hightlightsData = input< DashboardData | null >(null);
 }
---
+++ src/app/features/dashboard/components/dashboard-highlights/highlight-card/highlight-card.ts
@@ -0,0 +1,15 @@
+import { Component, input } from '@angular/core';
+import { Coin } from '../../../../../core/types/coin-types';
+import { CommonModule } from '@angular/common';
+import { PriceHighlightDirective } from '../../../../../shared/directives/price-highlight-directive';
+import { BenefitIndicator } from "../../../../../shared/directives/benefit-indicator";
+
+@Component({
+  selector: 'highlight-card',
+  imports: [CommonModule, PriceHighlightDirective, BenefitIndicator],
+  templateUrl: './highlight-card.html',
+  styleUrl: './highlight-card.css',
+})
+export class HighlightCard {
+  public readonly data = input< Coin | null >(null);
+}
