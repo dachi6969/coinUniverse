9c526bb1c2805a52d2f36e135611c21c038056a9
dachi <ani10sanadze@gmail.com>
2026-06-02 08:14:39 +0400

    feat: add output in  open-notification component, keep it dumb, lift it's states into service

--- src/app/features/user-notifications/components/open-notification/open-notification.ts
+++ src/app/features/user-notifications/components/open-notification/open-notification.ts
@@ -1,6 +1,5 @@
-import { Component, inject, input, OnInit, output } from '@angular/core';
+import { Component, input, OnInit, output } from '@angular/core';
 import { Notifications } from '../../../../core/types/notifications-types';
-import { UserNotifyService } from '../../../../core/services/user-notifications/user-notify-service';
 
 @Component({
   selector: 'open-notification',
@@ -9,27 +8,19 @@ import { UserNotifyService } from '../../../../core/services/user-notifications/
   styleUrl: './open-notification.css',
 })
 export class OpenNotification implements OnInit {
-  public goBack = output();
-  public message = input< Notifications | null >();
-
-  private nService = inject(UserNotifyService);
+  public readonly message = input.required<Notifications | null>();
+  public readonly goBack = output();
+  public readonly readNotifi = output<Notifications>();
   
-  ngOnInit(): void {
-    const isRead = this.message()?.is_read;
-
-    if ( isRead ) return;
-    this.readNotif();
+  ngOnInit() {
+    const message = this.message();
+    if ( !message ) return;
+    this.readNotifi.emit(message);
   }
 
   public onBack(event: MouseEvent): void {
     event.stopPropagation(); 
     this.goBack.emit();
   };
-
-  private readNotif(): void {
-    const msg = this.message();
-    if (!msg) return
-    this.nService.readNotification(msg?.id, msg.user_id);
-  }
 
 }
