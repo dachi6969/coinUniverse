1f433397ed21506eaa273f21960bfce6af77d563
dachi <ani10sanadze@gmail.com>
2026-06-02 08:12:50 +0400

    feat(service): lift up component-level states into service, make it as a source of truth

--- src/app/features/user-notifications/services/notifications-service.ts
+++ src/app/features/user-notifications/services/notifications-service.ts
@@ -1,18 +1,45 @@
-import { Injectable, signal } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
+import { UserNotifyService } from '../../../core/services/user-notifications/user-notify-service';
+import { Notifications } from '../../../core/types/notifications-types';
 
 
 @Injectable({
   providedIn: 'root',
 })
 export class NotificationsService {
-
+  // Local UI States.
   public isNotificationOpen = signal<boolean>(false);
+  public selectedMessage = signal< Notifications | null >(null);
+  
+  // Injected UI States.
+  private readonly userNotifyService = inject(UserNotifyService);
+  public readonly isLoading = this.userNotifyService.isLoading;
+  public readonly notifications = this.userNotifyService.notifications;
 
+
+  // METHODS.
   public open() {
     this.isNotificationOpen.set(true);
   };
   public close() {
     this.isNotificationOpen.set(false);
   };
-  
+
+  public deleteAllNotifications(): void {
+    this.userNotifyService.deleteAllNotifications();
+  };
+
+  public openMessage(notifi: Notifications) {
+    this.selectedMessage.set(notifi);
+    this.isNotificationOpen.set(true);
+  };
+
+  public closeMessage(): void {
+    this.isNotificationOpen.set(false);
+  };
+
+  public readNotification(msg: Notifications) {
+    this.userNotifyService.readNotification(msg.id,msg.user_id);
+  };
+
 }
