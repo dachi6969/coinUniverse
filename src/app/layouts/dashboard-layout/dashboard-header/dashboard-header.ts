2a82de55d4e3dd3f7e057ae02e852db7473634c0
dachi <ani10sanadze@gmail.com>
2026-07-02 20:40:01 +0400

    feat(header): add auth/button functional on header, simple changes in style, clean up ts file and lift up shared states in service

--- src/app/layouts/dashboard-layout/dashboard-header/dashboard-header.ts
+++ src/app/layouts/dashboard-layout/dashboard-header/dashboard-header.ts
@@ -1,51 +1,34 @@
-import { Component, effect, inject, OnDestroy, signal } from '@angular/core';
-import { MenuIcon } from "../../../shared/icons/menu-icon/menu-icon";
+import { Component, inject } from '@angular/core';
 import { DashboardSidebar } from "../dashboard-sidebar/dashboard-sidebar";
 import { ProfileIcon } from "../../../shared/icons/profile-icon/profile-icon";
-import { DashboardSearch } from "../components/dashboard-search/dashboard-search";
 import { DashboardLayoutService } from '../services/dashboard-layout-service';
-import { AuthService } from '../../../core/services/auth-services/auth-service';
-import {  NavigationEnd, Router } from '@angular/router';
-import { filter, Subscription } from 'rxjs';
+import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { LogoIcon } from "../../../shared/icons/logo-icon/logo-icon";
 
 @Component({
   selector: 'app-dashboard-header',
-  imports: [MenuIcon, DashboardSidebar, ProfileIcon, DashboardSearch, CommonModule],
+  imports: [
+    DashboardSidebar, 
+    ProfileIcon, 
+    CommonModule, 
+    LogoIcon, 
+    RouterLink
+  ],
   templateUrl: './dashboard-header.html',
   styleUrl: './dashboard-header.css',
 })
-export class DashboardHeader implements OnDestroy {
+export class DashboardHeader {
 
-  private router = inject(Router);
+  private readonly dashboardLayoutService = inject(DashboardLayoutService);
+
+  public readonly isDesktop = this.dashboardLayoutService.isDesktop;
+  public readonly isSidebarOpen = this.dashboardLayoutService.isSidebarOpen;
+  public readonly isUserLoggedIn = this.dashboardLayoutService.isUserLoggedIn;
+  public readonly headerTitle$ = this.dashboardLayoutService.headerTitle$;
 
 
-  private dashboardLayoutService = inject(DashboardLayoutService);
-  public isDesktop = this.dashboardLayoutService.isDesktop;
-  public isSidebarOpen = this.dashboardLayoutService.isSidebarOpen;
-
-  private authService = inject(AuthService);
-  private user = this.authService.userData;
-
-  public headerTitle = signal<string>('Dashboard');
-  sub!: Subscription;
-
-  constructor() {
-    this.sub = this.router.events
-    .pipe(
-      filter(event => event instanceof NavigationEnd)
-    )
-    .subscribe(() => {
-      const url = this.router.url;
-      const formated = url.slice(1)
-      this.headerTitle.set(formated);
-    });
-  }
-
-  ngOnDestroy(): void {
-    this.sub.unsubscribe();
-  }
-
+  // METHODS.
   public open(): void {
     this.dashboardLayoutService.openSidebar();
   }
@@ -54,13 +37,15 @@ export class DashboardHeader implements OnDestroy {
   }
 
   public navToProfile(): void {
-    const user = this.user();
+    this.dashboardLayoutService.navToProfile();
+  };
 
-    user ? 
-    this.router.navigate(['/profile']) :
-    this.authService.openAuthModal();
+  public navToMainPage(): void {
+    this.dashboardLayoutService.navToMainPage();
+  };
 
+  public logout(): void {
+    this.dashboardLayoutService.logout();
   }
-
 
 }
