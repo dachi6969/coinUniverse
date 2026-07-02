1acb0589b2874cd5ae9a7dc9d70548ca59bc7894
dachi <ani10sanadze@gmail.com>
2026-07-02 20:37:42 +0400

    refactor(sidebar): lift up state/methods in service, simple changes in style

--- src/app/layouts/dashboard-layout/dashboard-sidebar/dashboard-sidebar.ts
+++ src/app/layouts/dashboard-layout/dashboard-sidebar/dashboard-sidebar.ts
@@ -1,53 +1,55 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { LogoIcon } from "../../../shared/icons/logo-icon/logo-icon";
 import { DashboardLayoutService } from '../services/dashboard-layout-service';
 import { NgComponentOutlet } from '@angular/common';
-import { AuthService } from '../../../core/services/auth-services/auth-service';
-import { Router, RouterLink } from '@angular/router';
+import { RouterLink } from '@angular/router';
 import { LoginIcon } from "../../../shared/icons/login-icon/login-icon";
 import { navigation } from './sidebar-nav-list';
-import { DisableIfDirective } from './disable-if-directive';
+import { DisableIfDirective } from './directives/disable-if-directive';
+import { HideOnScrollDirective } from "./directives/hide-on-scroll-directive";
 
 @Component({
   selector: 'app-dashboard-sidebar',
   imports: [
-    LogoIcon, 
-    NgComponentOutlet, 
-    LoginIcon, 
-    RouterLink, 
-    DisableIfDirective
-  ],
+    LogoIcon,
+    NgComponentOutlet,
+    LoginIcon,
+    RouterLink,
+    DisableIfDirective,
+    HideOnScrollDirective
+],
   templateUrl: './dashboard-sidebar.html',
   styleUrl: './dashboard-sidebar.css',
 })
 export class DashboardSidebar {
-  private readonly router = inject(Router);
 
   private readonly dashboardLayoutService = inject(DashboardLayoutService);
+
   public readonly isDesktop = this.dashboardLayoutService.isDesktop;
+  private readonly notifications = this.dashboardLayoutService.notifications;
+  public readonly isUserLoggedIn = this.dashboardLayoutService.isUserLoggedIn;
 
-  private readonly authService = inject(AuthService);
-  public readonly isUserLoggedIn = 
-  this.authService.isUserLoggedIn;
+  public readonly navigation = navigation;
 
+  public readonly unreadNotify = computed(() => {
+    const notif = this.notifications();
+    if ( !notif ) return 0;
+    const notifQ = notif.filter(n => !n.is_read);
+    return notifQ.length;
+  });
+
+  // METHODS.
   public disableState (title: string): boolean {
     if ( this.isUserLoggedIn() ) return false;
     return title === 'Notifications' || title === 'Portfolio' 
   }
 
-  public readonly navigation = navigation;
-
   public closeSidebar(): void {
     this.dashboardLayoutService.closeSidebar();
   }
 
-  public navToLoginPage(): void {
-    this.router.navigate(['/auth/login']);
-  };
-
-  public logOut(): void {
-    this.authService.logoutUser();
-    this.closeSidebar();
+  public logout(): void {
+    this.dashboardLayoutService.logout();
   }
 
 }
